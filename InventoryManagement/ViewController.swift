d0818a1f298ece935af9ae85eea58358c6c0f311
Adria TImm <atimm13@winona.edu>
2020-01-28 11:11:55 -0600

    Initial Commit

---
+++ InventoryManagement/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  InventoryManagement
+//
+//  Created by Timm, Adria M on 1/28/20.
+//  Copyright © 2020 Timm, Adria M. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
